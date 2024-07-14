9346c6e1b22d38e1490f3f296d9a4df91810d5fd
Rose Owili <rosemonet61@gmail.com>
2024-07-14 12:45:05 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -4,9 +4,9 @@ function checkAnswer() {
   let userAnswer =  document.querySelector ('input[name="quiz"]:checked').value;
     
 if (userAnswer === correctAnswer) {
-  document.getElementById ("feedback").textContent = "Correct! Well Done.";
+  document.getElementById ("#feedback").textContent = "Correct! Well Done.";
 } else {
- document.getElementById ("feedback").textContent = "That's incorrect. Try again!";
+ document.getElementById ("#feedback").textContent = "That's incorrect. Try again!";
 }
 }
  document.getElementById ("submit-answer").addEventListener("click", checkAnswer); 
